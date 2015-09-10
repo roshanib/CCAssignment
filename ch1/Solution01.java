30163a62e4f6739395017cbdcb73e3c6b1f9809c
roshanib <roshani.bhandari@gmail.com>
2015-09-10 13:02:50 -0400

    trial solution

---
+++ ch1/Solution01.java
@@ -0,0 +1,14 @@
+// Probably use a bitmap which is sizeof(char)/8 in size
+ 
+ public class Solution01 {
+    public static void main(String args[]) {
+    
+        String check = "abcd";
+        System.out.println(isUnique(check));    
+
+    }
+
+    public static boolean isUnique(String str) {
+        return true;
+    }
+ }
