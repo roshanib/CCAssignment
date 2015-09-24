b38a9f3f7e7f3a006ca961f4b78fbea1cdfa7a6b
roshanib <roshani.bhandari@gmail.com>
2015-09-24 10:45:03 -0400

    Homework 2

--- ch4/Solution11.java
+++ ch4/Solution11.java
@@ -2,13 +2,13 @@ package ch4;
 
 import java.util.Random;
 
-class TreeNode {
+class MyNode {
 	public int val;
-	public TreeNode left;
-	public TreeNode right;
+	public MyNode left;
+	public MyNode right;
 	private int size = 0; // to figure where to get the random node from
 
-	public TreeNode(int v) {
+	public MyNode(int v) {
 		val = v;
 		size = 1;
 	}
@@ -16,13 +16,13 @@ class TreeNode {
 	public void insert(int v) {
 		if (v <= val) {
 			if (left == null) {
-				left = new TreeNode(v);
+				left = new MyNode(v);
 			} else {
 				left.insert(v);
 			}
 		} else {
 			if (right == null) {
-				right = new TreeNode(v);
+				right = new MyNode(v);
 			} else {
 				right.insert(v);
 			}
@@ -30,7 +30,7 @@ class TreeNode {
 		size++;
 	}
 
-	public TreeNode find(int v) {
+	public MyNode find(int v) {
 		if (v == val) {
 			return this;
 		} else if (v <= val) {
@@ -41,7 +41,7 @@ class TreeNode {
 		return null;
 	}
 
-	public TreeNode getRandomNode() {
+	public MyNode getRandomNode() {
 		int leftSize = left == null ? 0 : left.size;
 		Random random = new Random();
 		int index = random.nextInt(size);
@@ -58,7 +58,7 @@ class TreeNode {
 public class Solution11 {
 
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(5);
+		MyNode root = new MyNode(5);
 		root.insert(6);
 		root.insert(1);
 		root.insert(1);
